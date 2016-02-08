05a22f21bde78481f561780edfd1bcd1b687923c
jareda <jareda@localhost>
2016-02-08 16:35:19 -0500

    Initial Commit

---
+++ JavaProof/src/edu/saintjoe/cs/jareda/javaproof/JavaProof.java
@@ -0,0 +1,11 @@
+package edu.saintjoe.cs.jareda.javaproof;
+
+public class JavaProof {
+
+	public static void main(String[] args) {
+		System.out.println("Ihave conquered this task");
+		// TODO Auto-generated method stub
+
+	}
+
+}
